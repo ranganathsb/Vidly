bf9dc764fa38949bbce5d78aa27e70a6ee213c29
Ranganath S <ranganathsb@gmail.com>
2019-06-20 15:32:55 +0530

    Excercise Add New/Edit feature for Movie

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -23,6 +23,50 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel{
+                Movie=movie,
+                Genres=_context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.InStock = movie.InStock;
+               
+
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
         public ViewResult Index()
         {
             var movies = _context.Movies.Include(m => m.Genre).ToList();
