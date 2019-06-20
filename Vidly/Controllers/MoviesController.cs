4bd593b18345ac967bc4b118dd6487efb9132d7f
Ranganath S <ranganathsb@gmail.com>
2019-06-20 13:02:28 +0530

    Exercise 3.3 Display the list of movies and their details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,20 +11,34 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
 
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
-        public IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie> { new Movie { Id = 1, Name = "Shrek!" }, new Movie { Id = 2, Name = "Wall-E" } };
-        }
+        //public IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie> { new Movie { Id = 1, Name = "Shrek!" }, new Movie { Id = 2, Name = "Wall-E" } };
+        //}
 
+        public ViewResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            return View(movie);
+        }
      
         // GET: Movies
         public ActionResult Random()
