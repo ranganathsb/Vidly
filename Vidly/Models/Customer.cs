b07e2a218ee86ed8847544d1e89fa06855b8045f
Ranganath S <ranganathsb@gmail.com>
2019-06-21 11:52:36 +0530

    Validation Course Activity

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage="Please enter Customer's Name!")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -22,6 +22,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name="Date Of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == MembershipType.Unknown || customer.MembershipTypeId == MembershipType.PayAsYouGo)
+            {
+                return ValidationResult.Success;
+            }
+            if (customer.BirthDate == null)
+            {
+                return new ValidationResult("BirthDate is Required.");
+            }
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return (age > 18) ? ValidationResult.Success : new ValidationResult("Customer Should be atleast 18 yrs old!");
+        }
+    }
+}
